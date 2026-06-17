e06de6c2a815f0b218bb55d37ecb157432190d77
Sanjay Bharathi <sanjay.bharathi7@gmail.com>
2026-06-16 23:50:46 -0500

    initial commmit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Flashcard from './components/Flashcard'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Flashcard/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Flashcard.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './Flashcard.css'
+
+
+const Flashcard = () => {
+    return (
+        <div>
+            <h2>Demo</h2>
+            <img src="https://preview.redd.it/where-is-this-one-image-from-its-the-most-popular-image-of-v0-11fmbftmqt3e1.jpeg?auto=webp&s=fb0562b65025f16df59e8cbacfe28adbd0532420"/>
+        </div>
+    )
+}
+
+export default Flashcard
